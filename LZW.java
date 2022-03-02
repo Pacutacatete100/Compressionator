8b44f452e17d3365f635c6f0f4bdffa8b9e34b2e
vartmina <vminatiskan12@gmail.com>
2022-03-02 14:17:37 -0600

    LZW

--- LZW.java
+++ LZW.java
@@ -1,5 +1,69 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class LZW {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    private static final int R = 256;
+    private static final int L = 4096;
+    private static final int W = 32;
+    
+
+    private LZW() { 
+
+    /**
+     * Reads a sequence of 8-bit bytes from standard input; compresses
+     * them using LZW compression with 12-bit codewords; and writes the results
+     * to standard output.
+     */
+    public static void compress() { 
+    	BufferedReader input = new BufferedReader(new FileReader(""));
+		String line = input.readLine();
+        TST<Integer> st = new TST<Integer>();
+
+        // since TST is not balanced, it would be better to insert in a different order
+        for (int i = 0; i < R; i++)
+            st.put("" + (char) i, i);
+
+        int code = R+1;  // R is codeword for EOF
+
+        while (input.length() > 0) {
+            String s = st.longestPrefixOf(input);  // Find max prefix match s.
+            input.write(st.get(s), W);      // Print s's encoding.
+            int t = s.length();
+            if (t < input.length() && code < L)    // Add s to symbol table.
+                st.put(input.substring(0, t + 1), code++);
+            input = input.substring(t);            // Scan past s in input.
+        }
+        input.write(R, W);
+        input.close();
+    } 
+
+    /**
+     * Reads a sequence of bit encoded using LZW compression with
+     * 12-bit codewords from standard input; expands them; and writes
+     * the results to standard output.
+     */
+    public static void expand() {
+        String[] st = new String[L];
+        int i; // next available codeword value
+
+        // initialize symbol table with all 1-character strings
+        for (i = 0; i < R; i++)
+            st[i] = "" + (char) i;
+        st[i++] = "";                        // (unused) lookahead for EOF
+
+        int codeword = input.readInt(W);
+        if (codeword == R) return;           // expanded message is empty string
+        String val = st[codeword];
+
+        while (true) {
+            input.write(val);
+            codeword = input.readInt(W);
+            if (codeword == R) break;
+            String s = st[codeword];
+            if (i == codeword) s = val + val.charAt(0);   // special case hack
+            if (i < L) st[i++] = val + s.charAt(0);
+            val = s;
+        }
+        input.close();
     }
-}
+    }
